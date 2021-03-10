078e0a1be30c3d4f2b368480bcb7ea4c766620df
AntoMullen <steriomullen@hotmail.com>
2021-03-10 12:23:33 +0000

    Section 3 Angular with bootstrap

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'My cool dating app';
+  users: any;
+  //inject the http component in constructor
+  constructor(private http: HttpClient){}
+
+  //This is an angular lifecycle method that fires after the constructor
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers()
+  {
+    this.http.get("https://localhost:5001/api/users").subscribe(response =>
+      {
+        this.users = response;
+        console.log(response);
+      }, 
+      error =>
+      {
+        console.log(error);
+      })
+  }
 }
